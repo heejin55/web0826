00cbdc48adbf0c1ca4a9d55523f2603a094322d1
heejin55 <sandmute@naver.com>
2024-08-30 09:57:01 +0900

    n
    n

--- cdn/src/02_render.js
+++ cdn/src/02_render.js
@@ -14,36 +14,30 @@
 */
 
 
-// 컴포넌트 생성
-function Hello() {
-    // 리턴 뒤에 한 줄 문장;
-    return <h1>Hello World!</h1>
-}
+// 변수 생성
+// 여기서 값은 JSX
+const hello = <h1>Hello World!</h1>;
+
+const hi =
+  <div>
+    <h1>Hi World!</h1>
+    <h2>나는 두 문장이다!</h2>
+  </div>
 
 
 /* 
-    // 라이브러리 이름: React, ReactDOM(가상돔), Babel(JSX번역)
+    // 라이브러리 이름: React, ReactDOM(가상돔), Babel(JSX번역) -> CDN 방식
     
     // 리액트의 출력
     // 객체.메서드
     ReactDOM.render(요소, 컨테이너); 
       1. ReactDOM은 가상 DOM을 생성하는 객체
       2. render()은 요소를 컨테이너에 넣는 메서드
-      3. 요소는 컴포넌트의 리턴 값을 XML의 빈 요소로 쓴다.
-        예) <Hello />
+      3. element는 컴포넌트명 또는 변수명을 빈 요소로 쓴다.
+        예) <Hello />, <br />>, <hr />, <img />, <input />
       4. 컨테이너는 HTML 페이지의 div#root를 말한다.
 */
 
 // 출력 -> 렌더링
-// ReactDOM.render(<App />, document.querySelector("#root"));
-
-ReactDOM.render(
-    <div style={{ display: "flex" }}>
-        <Hello />
-        <Hello />
-        <Hello />
-        <Hello />
-    </div>,
-    document.querySelector("#root")
-);
+ReactDOM.render(<hello />, document.getElementById("#root"));
 
